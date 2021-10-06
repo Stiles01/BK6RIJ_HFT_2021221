4dc655f8575b02776fd2af5c9f92401bd8e2aa0e
nagyv <nagyv@DESKTOP-JO37MHM>
2021-10-06 09:01:03 +0200

    Add project files.

---
+++ BK6RIJ_HFT_2021221.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BK6RIJ_HFT_2021221.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ BK6RIJ_HFT_2021221.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BK6RIJ_HFT_2021221.Data
+{
+    public class Class1
+    {
+    }
+}
