484fb8746d8ed1b561ea70320cde857d8422c111
Nagy Viktória <nagyv0730@stud.uni-obuda.hu>
2021-10-14 12:05:22 +0200

    XYZDbContext created, nuget packages dowloaded and data loaded

---
+++ BK6RIJ_HFT_2021221.Data/XYZDbContext.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using BK6RIJ_HFT_2021221.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BK6RIJ_HFT_2021221.Data
+{
+    public class XYZDbContext : DbContext
+    {
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Delivery> Deliveries { get; set; }
+
+        public XYZDbContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            if (!builder.IsConfigured)
+            {
+                string conn =
+                    @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\CarDB.mdf;Integrated Security=True";
+                builder
+                    .UseLazyLoadingProxies()
+                    .UseSqlServer(conn);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity
+                .HasOne(order => order.Customer)
+                .WithMany(customer => customer.Orders)
+                .HasForeignKey(order => order.CustomerId)
+                .OnDelete(DeleteBehavior.Restrict);
+            });
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity
+                .HasOne(order => order.Product)
+                .WithMany(product => product.Orders)
+                .HasForeignKey(order => order.ProductId)
+                .OnDelete(DeleteBehavior.Restrict);
+            });
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity
+                .HasOne(order => order.Delivery)
+                .WithMany(delivery => delivery.Orders)
+                .HasForeignKey(order => order.DeliveryId)
+                .OnDelete(DeleteBehavior.Restrict);
+            });
+
+            Delivery fox = new Delivery { Id = 1, Company = "FoxPost", DeliveryDays = 6 };
+            Delivery gls = new Delivery { Id = 2, Company = "GLS", DeliveryDays = 3 };
+            Delivery exp = new Delivery { Id = 3, Company = "ExpressOne", DeliveryDays = 2 };
+            Delivery post = new Delivery { Id = 4, Company = "Hungarian Post", DeliveryDays = 14 };
+
+            Product p1 = new Product { Id = 1, Name = "Bread", Price = 300 };
+            Product p2 = new Product { Id = 2, Name = "Milk", Price = 200 };
+            Product p3 = new Product { Id = 3, Name = "Headset", Price = 10000 };
+            Product p4 = new Product { Id = 4, Name = "Flower", Price = 3000 };
+            Product p5 = new Product { Id = 5, Name = "Cake", Price = 5632 };
+            Product p6 = new Product { Id = 6, Name = "Shoes", Price = 20000 };
+
+            Customer c1 = new Customer { Id = 1, FirstName = "Kovacs", LastName = "Bela" };
+            Customer c2 = new Customer { Id = 2, FirstName = "Kovacs", LastName = "Erzsebet" };
+            Customer c3 = new Customer { Id = 3, FirstName = "Nagy", LastName = "Pistike" };
+            Customer c4 = new Customer { Id = 4, FirstName = "Varga", LastName = "Virag" };
+
+            Order o1 = new Order { OrderId = 1, CustomerId = 4, ProductId = 4, DeliveryId = 3, OrderDate = new DateTime(2021, 10, 14) };
+            Order o2 = new Order { OrderId = 2, CustomerId = 2, ProductId = 5, DeliveryId = 2, OrderDate = new DateTime(2021, 07, 26) };
+            Order o3 = new Order { OrderId = 3, CustomerId = 2, ProductId = 3, DeliveryId = 4, OrderDate = new DateTime(2021, 07, 16) };
+            Order o4 = new Order { OrderId = 4, CustomerId = 3, ProductId = 6, DeliveryId = 1, OrderDate = new DateTime(2021, 03, 04) };
+            Order o5 = new Order { OrderId = 5, CustomerId = 1, ProductId = 1, DeliveryId = 2, OrderDate = new DateTime(2021, 09, 30) };
+
+
+            modelBuilder.Entity<Delivery>().HasData(fox, gls, exp, post);
+            modelBuilder.Entity<Product>().HasData(p1, p2, p3, p4, p5, p6);
+            modelBuilder.Entity<Customer>().HasData(c1, c2, c3, c4);
+            modelBuilder.Entity<Order>().HasData(o1, o2, o3, o4, o5);
+        }
+    }
+}
