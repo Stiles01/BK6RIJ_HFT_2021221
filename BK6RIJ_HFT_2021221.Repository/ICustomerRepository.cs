e330dd9e39dc63698216a506c78cedfa170c9176
Nagy Viktória <nagyv0730@stud.uni-obuda.hu>
2021-11-05 09:49:20 +0100

    ICustomerRepository created and done

---
+++ BK6RIJ_HFT_2021221.Repository/ICustomerRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using BK6RIJ_HFT_2021221.Models;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BK6RIJ_HFT_2021221.Repository
+{
+    interface ICustomerRepository
+    {
+        void Create(Customer customer);
+        void Delete(int id);
+        Customer Read(int id);
+        IQueryable<Customer> ReadAll();
+        void Update(Customer customer);
+    }
+}
