5b71bd52f5d07eeed077d69066a04c2c51e29807
Nagy Viktória <nagyv0730@stud.uni-obuda.hu>
2021-11-05 10:11:58 +0100

    DeliveryRepository created with CRUD methods implemented

---
+++ BK6RIJ_HFT_2021221.Repository/DeliveryRepository.cs
@@ -0,0 +1,52 @@
+﻿using BK6RIJ_HFT_2021221.Data;
+using BK6RIJ_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BK6RIJ_HFT_2021221.Repository
+{
+    class DeliveryRepository : IDeliveryRepository
+    {
+        XYZDbContext db;
+
+        public DeliveryRepository(XYZDbContext XYZDb)
+        {
+            db = XYZDb;
+        }
+
+
+
+        public void Create(Delivery delivery)
+        {
+            db.Deliveries.Add(delivery);
+            db.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            db.Remove(Read(id));
+            db.SaveChanges();
+        }
+
+        public Delivery Read(int id)
+        {
+            return db.Deliveries.FirstOrDefault(x => x.Id == id);
+        }
+
+        public IQueryable<Delivery> ReadAll()
+        {
+            return db.Deliveries;
+        }
+
+        public void Update(Delivery delivery)
+        {
+            var oldDelivery = Read(delivery.Id);
+            oldDelivery.DeliveryDays = delivery.DeliveryDays;
+            oldDelivery.Company = delivery.Company;
+            db.SaveChanges();
+        }
+    }
+}
