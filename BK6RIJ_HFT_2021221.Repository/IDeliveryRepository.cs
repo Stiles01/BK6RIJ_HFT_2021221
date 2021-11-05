aeea89591d0e386cf617e680b94c50afeb278a7e
Nagy Viktória <nagyv0730@stud.uni-obuda.hu>
2021-11-05 09:53:07 +0100

    IDeliveryRepository created and done

---
+++ BK6RIJ_HFT_2021221.Repository/IDeliveryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using BK6RIJ_HFT_2021221.Models;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BK6RIJ_HFT_2021221.Repository
+{
+    interface IDeliveryRepository
+    {
+        void Create(Delivery delivery);
+        void Delete(int id);
+        Delivery Read(int id);
+        IQueryable<Delivery> ReadAll();
+        void Update(Delivery delivery);
+    }
+}
