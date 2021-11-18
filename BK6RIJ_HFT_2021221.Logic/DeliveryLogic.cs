2d9b50d338417a3c1699cb19e8ac99a9a33f2476
Nagy Viktória <nagyv0730@stud.uni-obuda.hu>
2021-11-18 09:01:39 +0100

    DeliveryLogic CRUD methods created

--- BK6RIJ_HFT_2021221.Logic/DeliveryLogic.cs
+++ BK6RIJ_HFT_2021221.Logic/DeliveryLogic.cs
@@ -8,7 +8,49 @@ using System.Threading.Tasks;
 
 namespace BK6RIJ_HFT_2021221.Logic
 {
-    public class DeliveryLogic
+    public class DeliveryLogic : IDeliveryLogic
     {
+        IDeliveryRepository deliveryRepo;
+
+        public DeliveryLogic(IDeliveryRepository delivery)
+        {
+            deliveryRepo = delivery;
+        }
+
+
+        public void Create(Delivery delivery)
+        {
+            if (delivery.Company == "")
+            {
+                throw new ArgumentNullException();
+            }
+            else if (delivery.DeliveryDays <= 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            deliveryRepo.Create(delivery);
+        }
+
+        public void Delete(int id)
+        {
+            deliveryRepo.Delete(id);
+        }
+
+        public Delivery Read(int id)
+        {
+            return deliveryRepo.Read(id);
+        }
+
+        public IQueryable<Delivery> ReadAll()
+        {
+            return deliveryRepo.ReadAll();
+        }
+
+        public void Update(Delivery delivery)
+        {
+            deliveryRepo.Update(delivery);
+        }
+
+        //non-CRUD
     }
 }
