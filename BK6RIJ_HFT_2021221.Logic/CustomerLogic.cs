bba5a96ced7764b08deefd4abe8a652b74b5039d
Nagy Viktória <nagyv0730@stud.uni-obuda.hu>
2021-11-18 09:06:19 +0100

    The CRUD methods of Customerlogic are created

--- BK6RIJ_HFT_2021221.Logic/CustomerLogic.cs
+++ BK6RIJ_HFT_2021221.Logic/CustomerLogic.cs
@@ -8,7 +8,43 @@ using System.Threading.Tasks;
 
 namespace BK6RIJ_HFT_2021221.Logic
 {
-    public class CustomerLogic
+    public class CustomerLogic : ICustomerLogic
     {
+        ICustomerRepository customerRepo;
+
+        public CustomerLogic(ICustomerRepository customer)
+        {
+            customerRepo = customer;
+        }
+
+
+        public void Create(Customer customer)
+        {
+            if (customer.FirstName == "" || customer.LastName == "")
+            {
+                throw new ArgumentNullException();
+            }
+            customerRepo.Create(customer);
+        }
+
+        public void Delete(int id)
+        {
+            customerRepo.Delete(id);
+        }
+
+        public Customer Read(int id)
+        {
+            return customerRepo.Read(id);
+        }
+
+        public IQueryable<Customer> ReadAll()
+        {
+            return customerRepo.ReadAll();
+        }
+
+        public void Update(Customer customer)
+        {
+            customerRepo.Update(customer);
+        }
     }
 }
