ccddda830955eaa4d9f77444e919b3d73674ac4d
Nagy Viktória <nagyv0730@stud.uni-obuda.hu>
2021-11-18 08:36:54 +0100

    The CRUD methods of OrderLogic are created

--- BK6RIJ_HFT_2021221.Logic/OrderLogic.cs
+++ BK6RIJ_HFT_2021221.Logic/OrderLogic.cs
@@ -8,7 +8,46 @@ using System.Threading.Tasks;
 
 namespace BK6RIJ_HFT_2021221.Logic
 {
-    public class OrderLogic
+    public class OrderLogic : IOrderLogic
     {
+        IOrderRepository orderRepo;
+
+        public OrderLogic(IOrderRepository order)
+        {
+            orderRepo = order;
+        }
+
+
+        public void Create(Order order)
+        {
+            Type t = order.OrderDate.GetType();
+            if (t.Equals(typeof(DateTime)))
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            orderRepo.Delete(id);
+        }
+
+        public Order Read(int id)
+        {
+            return orderRepo.Read(id);
+        }
+
+        public IQueryable<Order> ReadAll()
+        {
+            return orderRepo.ReadAll();
+        }
+
+        public void Update(Order order)
+        {
+            orderRepo.Update(order);
+        }
+
+
+        //non-CRUD
     }
 }
