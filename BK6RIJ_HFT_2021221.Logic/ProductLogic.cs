93e1a280d327f2656d816c34d36c9a5396f32d96
Nagy Viktória <nagyv0730@stud.uni-obuda.hu>
2021-11-18 08:09:53 +0100

    The CRUD methods in ProductLogic created.

--- BK6RIJ_HFT_2021221.Logic/ProductLogic.cs
+++ BK6RIJ_HFT_2021221.Logic/ProductLogic.cs
@@ -8,7 +8,51 @@ using System.Threading.Tasks;
 
 namespace BK6RIJ_HFT_2021221.Logic
 {
-    public class ProductLogic
+    public class ProductLogic : IProductLogic
     {
+        IProductRepository productRepo;
+
+        public ProductLogic(IProductRepository p)
+        {
+            productRepo = p;
+        }
+
+        public void Create(Product product)
+        {
+            if (product.Name == "")
+            {
+                throw new ArgumentNullException();
+            }
+            else if (product.Price <= 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            productRepo.Create(product);
+        }
+
+        public void Delete(int id)
+        {
+            productRepo.Delete(id);
+        }
+
+        public Product Read(int id)
+        {
+            return productRepo.Read(id);
+        }
+
+        public IQueryable<Product> ReadAll()
+        {
+            return productRepo.ReadAll();
+        }
+
+        public void Update(Product product)
+        {
+            productRepo.Update(product);
+        }
+
+        //non-CRUD
+
+
+
     }
 }
