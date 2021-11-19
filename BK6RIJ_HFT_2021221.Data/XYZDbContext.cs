0d1155703844660faa4c1e269b44eb3274f5b988
Nagy Viktória <nagyv0730@stud.uni-obuda.hu>
2021-11-19 09:22:01 +0100

    more orders added to the database

--- BK6RIJ_HFT_2021221.Data/XYZDbContext.cs
+++ BK6RIJ_HFT_2021221.Data/XYZDbContext.cs
@@ -76,17 +76,36 @@ namespace BK6RIJ_HFT_2021221.Data
             Customer c3 = new Customer { Id = 3, FirstName = "Nagy", LastName = "Pistike" };
             Customer c4 = new Customer { Id = 4, FirstName = "Varga", LastName = "Virag" };
 
-            Order o1 = new Order { OrderId = 1, CustomerId = 4, ProductId = 4, DeliveryId = 3, OrderDate = new DateTime(2021, 10, 14) };
-            Order o2 = new Order { OrderId = 2, CustomerId = 2, ProductId = 5, DeliveryId = 2, OrderDate = new DateTime(2021, 07, 26) };
-            Order o3 = new Order { OrderId = 3, CustomerId = 2, ProductId = 3, DeliveryId = 4, OrderDate = new DateTime(2021, 07, 16) };
-            Order o4 = new Order { OrderId = 4, CustomerId = 3, ProductId = 6, DeliveryId = 1, OrderDate = new DateTime(2021, 03, 04) };
-            Order o5 = new Order { OrderId = 5, CustomerId = 1, ProductId = 1, DeliveryId = 2, OrderDate = new DateTime(2021, 09, 30) };
+            List<Order> orders = new List<Order>()
+            {
+                new Order { OrderId = 1, CustomerId = 4, ProductId = 4, DeliveryId = 3, OrderDate = new DateTime(2021, 10, 14) },
+                new Order { OrderId = 2, CustomerId = 2, ProductId = 5, DeliveryId = 2, OrderDate = new DateTime(2021, 07, 26) },
+                new Order { OrderId = 3, CustomerId = 2, ProductId = 3, DeliveryId = 4, OrderDate = new DateTime(2021, 07, 16) },
+                new Order { OrderId = 4, CustomerId = 3, ProductId = 6, DeliveryId = 1, OrderDate = new DateTime(2021, 03, 04) },
+                new Order { OrderId = 5, CustomerId = 1, ProductId = 1, DeliveryId = 2, OrderDate = new DateTime(2021, 09, 30) },
+                new Order { OrderId = 6, CustomerId = 4, ProductId = 4, DeliveryId = 3, OrderDate = new DateTime(2021, 10, 30) },
+                new Order { OrderId = 7, CustomerId = 3, ProductId = 3, DeliveryId = 1, OrderDate = new DateTime(2021, 11, 30) },
+                new Order { OrderId = 8, CustomerId = 2, ProductId = 5, DeliveryId = 2, OrderDate = new DateTime(2021, 08, 30) },
+                new Order { OrderId = 9, CustomerId = 1, ProductId = 2, DeliveryId = 1, OrderDate = new DateTime(2021, 07, 30) },
+                new Order { OrderId = 10, CustomerId = 1, ProductId = 1, DeliveryId = 2, OrderDate = new DateTime(2020, 06, 30) },
+                new Order { OrderId = 1, CustomerId = 4, ProductId = 4, DeliveryId = 3, OrderDate = new DateTime(2020, 10, 14) },
+                new Order { OrderId = 2, CustomerId = 2, ProductId = 5, DeliveryId = 2, OrderDate = new DateTime(2020, 07, 26) },
+                new Order { OrderId = 3, CustomerId = 2, ProductId = 3, DeliveryId = 4, OrderDate = new DateTime(2020, 07, 16) },
+                new Order { OrderId = 4, CustomerId = 3, ProductId = 6, DeliveryId = 1, OrderDate = new DateTime(2020, 03, 04) },
+                new Order { OrderId = 5, CustomerId = 1, ProductId = 1, DeliveryId = 2, OrderDate = new DateTime(2020, 09, 30) },
+                new Order { OrderId = 6, CustomerId = 4, ProductId = 4, DeliveryId = 3, OrderDate = new DateTime(2020, 10, 30) },
+                new Order { OrderId = 7, CustomerId = 3, ProductId = 3, DeliveryId = 1, OrderDate = new DateTime(2020, 11, 30) },
+                new Order { OrderId = 8, CustomerId = 2, ProductId = 5, DeliveryId = 2, OrderDate = new DateTime(2020, 08, 30) },
+                new Order { OrderId = 9, CustomerId = 1, ProductId = 2, DeliveryId = 1, OrderDate = new DateTime(2020, 07, 30) },
+                new Order { OrderId = 10, CustomerId = 1, ProductId = 1, DeliveryId = 2, OrderDate = new DateTime(2020, 06, 30) },
+            };
+              
 
 
             modelBuilder.Entity<Delivery>().HasData(fox, gls, exp, post);
             modelBuilder.Entity<Product>().HasData(p1, p2, p3, p4, p5, p6);
             modelBuilder.Entity<Customer>().HasData(c1, c2, c3, c4);
-            modelBuilder.Entity<Order>().HasData(o1, o2, o3, o4, o5);
+            modelBuilder.Entity<Order>().HasData(orders);
         }
     }
 }
