9dcd8b7cf09a6bc22d832c42f0f0725485b93bf3
nagyv0730@stud.uni-obuda.hu <nagyv0730@stud.uni-obuda.hu>
2022-04-10 12:53:38 +0200

    ICommands, but delete doesn't work

--- BK6RIJ_HFT_2021221.WPFClient/ViewModels/MainWindowViewModel.cs
+++ BK6RIJ_HFT_2021221.WPFClient/ViewModels/MainWindowViewModel.cs
@@ -1,19 +1,53 @@
 ﻿using BK6RIJ_HFT_2021221.Models;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace BK6RIJ_HFT_2021221.WPFClient.ViewModels
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : ObservableRecipient
     {
         public RestCollection<Customer> Customers { get; set; }
+
+        private Customer selectedCustomer;
+
+        public Customer SelectedCustomer
+        {
+            get { return selectedCustomer; }
+            set 
+            { 
+                SetProperty(ref selectedCustomer, value);
+                (DeleteCustomerCommand as RelayCommand).NotifyCanExecuteChanged();
+                //(UpdateCustomerCommand as RelayCommand).NotifyCanExecuteChanged();
+            }
+        }
+
+
+        public ICommand CreateCustomerCommand { get; set; }
+        public ICommand DeleteCustomerCommand { get; set; }
+        public ICommand UpdateCustomerCommand { get; set; }
 
         public MainWindowViewModel()
         {
             Customers = new RestCollection<Customer>("http://localhost:9973/", "customer");
+            CreateCustomerCommand = new RelayCommand(() => 
+            {
+                Customers.Add(new Customer()
+                {
+                    FirstName = "FirstName",
+                    LastName = "LastName"
+                });
+            });
+
+            DeleteCustomerCommand = new RelayCommand(() => 
+            {
+                Customers.Delete(selectedCustomer.Id);
+            }, () => SelectedCustomer != null);
         }
     }
 }
