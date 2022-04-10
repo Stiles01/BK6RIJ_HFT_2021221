33d1cd845f8ae10e379bf44d4b3d6e5dad6a5cfd
nagyv0730@stud.uni-obuda.hu <nagyv0730@stud.uni-obuda.hu>
2022-04-10 12:25:17 +0200

    WPF main window works for customers (multibinding)

---
+++ BK6RIJ_HFT_2021221.WPFClient/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using BK6RIJ_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BK6RIJ_HFT_2021221.WPFClient.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        public RestCollection<Customer> Customers { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Customers = new RestCollection<Customer>("http://localhost:9973/", "customer");
+        }
+    }
+}
